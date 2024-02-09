b7320cd0537a9ad31a482d58cc77f83ef81a2afb
VladimirKovach <150423755+VladimirKovach@users.noreply.github.com>
2024-02-09 09:53:07 +0000

    XOR

--- src/Cryto.java
+++ src/Cryto.java
@@ -1,6 +1,6 @@
 public class Cryto {
 
-    static protected String Cesar(String data, int shift) {
+    public static String Cesar(String data, int shift) {
         StringBuilder output = new StringBuilder(data.length());
         for (char singleChar : data.toCharArray()) {
             int shiftedChar = (int) (singleChar + shift) % 256;
@@ -9,14 +9,14 @@ public class Cryto {
         return output.toString();
     }
 
-    static private String xorAlgo(String data, String key) {
+    public static String xorAlgo(String data, String key) {
 
         String acc = "";
-
-        for (int i = 0; i < data.length(); i++) {
+        
+        for (int i = 0; i < data.length();) {
             char d = data.charAt(i);
 
-            for (int j = 0; j < key.length(); j++) {
+            for (int j = 0; j < key.length() && i < data.length(); j++, i++) {
                 char k = key.charAt(j);
 
                 char xorChar = (char) (d ^ k);
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ciao tizio!");
+
+        System.out.println("Inserisci il messagio da crtitare: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        
+        String key = "kap";
+        scanner.close();
+
+        String strCrypted = Cryto.xorAlgo(str, key);
+        System.out.println(strCrypted);
+        System.out.println(Cryto.xorAlgo(strCrypted, key));
+
+        System.out.println("---------------------------------------");
+        int keyInt = 5;
+        strCrypted = Cryto.Cesar(str, keyInt);
+        System.out.println(strCrypted);
+        System.out.println(Cryto.Cesar(strCrypted, -keyInt));
     }
 }
